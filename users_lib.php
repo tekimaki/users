286decd27391ddf560457e1012fd143c478f1e11
tekimaki <will@tekimaki.com>
2010-09-29 16:27:02 -0400

    fetch a httpauth tpl from themes and display only it - not a full site since this is a 401 user should get nothing about the site

--- users_lib.php
+++ users_lib.php
@@ -145,9 +145,12 @@ function users_httpauth ( $pRequireSSL = FALSE ){
 	}
 	// require http auth
 	else{
+		global $gBitSmarty;
+		$gBitSmarty->assign_by_ref('gBitSystem', $gBitSystem);
 		header('WWW-Authenticate: Basic realm="'.$gBitSystem->getConfig('site_title').'"');
 		header('HTTP/1.0 401 Unauthorized');
-		$gBitSystem->fatalError( tra('HTTP Authentication Canceled') );
+		$msg = $gBitSmarty->fetch('bitpackage:themes/httpauth_required.tpl');
+		echo $msg;
 		exit;
 	}
 }
